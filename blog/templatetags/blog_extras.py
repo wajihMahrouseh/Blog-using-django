4c2340554c2374d360d790eebf6509b1a6510a21
wajihma <wajiho.mahrouseh@gmail.com>
2022-03-05 16:28:55 +0200

    add custom template tags
    - simple template tags
    - access template context
    - inclusion template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,6 +2,7 @@ from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
 
+from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
@@ -19,7 +20,7 @@ def author_details(author, current_user=None):
         check if user has a first and last name --> return the first and last name else return the username
         also check if user has email --> return an anchor tag to mail him/her else return the name
     """
-    
+
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
@@ -41,3 +42,58 @@ def author_details(author, current_user=None):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+
+
+# simple template tags
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html('</div>')
+
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+
+# tempalte tags that access to  template context
+# jsut for test --> {% author_details_tag %}
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
+    
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+    
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">',
+        author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+    
+    return format_html("{}{}{}", prefix, name, suffix)
+
+
+# inclusion tags
+@register.inclusion_tag("blog/post_list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name="home-page"),
+    path("post/<slug>/", views.post_detail, name="blog-post-detail"),
 ]
